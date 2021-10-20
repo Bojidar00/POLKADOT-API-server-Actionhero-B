ee95b03bb172d09b1289dac3a1c1a7303e140e47
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-10-20 21:02:54 +0300

    Small fix

--- __tests__/actions/status.ts
+++ __tests__/actions/status.ts
@@ -7,23 +7,8 @@ const swaggerDocument = require('../../swagger.json');
 
 jestOpenAPI(swaggerDocument);
 
-describe("actionhero Tests", () => {
-  const actionhero = new Process();
 
-  beforeAll(async () => await actionhero.start());
-  afterAll(async () => await actionhero.stop());
 
-  test("should have booted into the test env", () => {
-    expect(process.env.NODE_ENV).toEqual("test");
-    expect(env).toEqual("test");
-    expect(id).toBeTruthy();
-  });
-
-  test("can retrieve server uptime via the status action", async () => {
-    const { uptime } = await specHelper.runAction<Status>("status");
-    expect(uptime).toBeGreaterThan(0);
-  });
-});
 
 describe('GET api/node/blocks', () => {
   it('should satisfy OpenAPI spec', async() => {
