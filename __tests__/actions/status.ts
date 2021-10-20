fe1835367c293943eaf5eac6120ec4f92f9b4c6c
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-10-20 21:08:38 +0300

    Small fix

--- __tests__/actions/status.ts
+++ __tests__/actions/status.ts
@@ -21,15 +21,3 @@ describe('GET api/node/blocks', () => {
   });
 });
 
-describe('GET /api/node/blocks/{x}/{nth}', () => {
-  it('should satisfy OpenAPI spec', async() => {
-      const x = 5;
-      const nth = 1512532;
-
-      const res = await axios.get(`http://0.0.0.0:8080/api/node/blocks/${x}/${nth}`);
-
-      expect(res.status).toEqual(200);
-
-      expect(res).toSatisfyApiSpec();
-  });
-});
